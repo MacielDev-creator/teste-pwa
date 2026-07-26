e5cca4577fefc77c6116cd2c1567920606f4007f
MacielDev-creator <macielmarketing2024@gmail.com>
2026-07-25 22:49:01 -0300

    Atualiza versao do service worker para v2

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ if ("serviceWorker" in navigator) {
         });
     });
 }
+
+// Mude de "pwa-v1" para "pwa-v2"
+const CACHE_NAME = "pwa-v2";
+
+const arquivos = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+// Adicione este evento para apagar os caches antigos automaticamente ao atualizar
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("install", event => {
+    self.skipWaiting(); // Força o novo Service Worker a ativar imediatamente
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(arquivos))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
